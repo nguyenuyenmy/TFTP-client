76a9385623c6359d701a2e3faf12dbd94c5b376f
Uyen-My <uyen-my.nguyen@ensea.fr>
2022-01-15 18:41:47 -0500

    Started recvfrom()

--- gettftp.c
+++ gettftp.c
@@ -9,26 +9,27 @@
 
 #define BSIZE 512
 #define OP_RRQ 1
+#define ACKSize 4
 
 //Definition of functions
 void enougharguments(int argc, char** argv, char **fileName, char **hostName);
-void displayMsg(char *message);
+void displayMsg(char *message); 
 int addrerror(int s);
 int createsocket(struct addrinfo *result);
 void * rrq_packet (char *filename, char *mode);
+int receive(int sock, char * packet, struct sockaddr sock_addr, int sendsize);
+char ACK(char * packet);
 
 
 int main(int argc, char** argv) {
 
 	// Verifying the entered arguments
-	
 	char *fileName;
 	char *hostName;
 	enougharguments(argc, argv, &fileName, &hostName);
 	
 	
 	// Getting the server address
-	
 	struct addrinfo hints, *result = NULL;
 	memset(&hints, 0, sizeof(struct addrinfo));
 
@@ -36,14 +37,13 @@ int main(int argc, char** argv) {
 	hints.ai_socktype = SOCK_DGRAM;
 	hints.ai_protocol = IPPROTO_UDP;
 	
-	char *port = "69";
+	char *port = "1069";
 	
 	int s = getaddrinfo(hostName, port, &hints, &result);
 	addrerror(s);
 	
 	
 	// Connecting socket to the server
-	
 	int sock = createsocket(result);
 
 	char *mode = "octet";
@@ -51,23 +51,38 @@ int main(int argc, char** argv) {
 	char * packet = rrq_packet(fileName, mode);
 	int totalLen = strlen(packet);
 	
-	/*printf("Hello\n");
-	displayMsg(packet);
-	printf("Hello\n");*/
-
-	int sendtono = sendto(sock, packet, totalLen, 0, result->ai_addr, result->ai_addrlen);
-	printf("%d\n", sendtono);
-	
-	if (sendtono != totalLen) {
+	if (sendto(sock, packet, totalLen, 0, result->ai_addr, result->ai_addrlen) != totalLen) {
       		perror ("Client: sendto has returned an error");
-      		exit (2);
+      		exit(EXIT_FAILURE);
       	}
 
+	
+	while(1) {
+		
+		struct sockaddr sock_addr;
+		int sendsize = sizeof(sock_addr);
+		
+		// Receiving one single data packet
+		int recvlen = receive(sock, packet, sock_addr, sendsize);
+		
+		
+		// ACK
+		char ack_packet = ACK(packet);
+		displayMsg(&ack_packet);
+		
+		if (sendto(sock, &ack_packet, ACKSize, 0, &sock_addr, sendsize) != totalLen) {
+      			perror ("Client: sendto has returned an error");
+      			exit(EXIT_FAILURE);
+      		}
+      	}
+      	
 	return EXIT_SUCCESS;
 }
 
 
+
 // Functions
+
 void enougharguments(int argc, char** argv, char **fileName, char **hostName) {
 
 	if (argc < 3) {
@@ -121,5 +136,29 @@ void * rrq_packet (char *filename, char *mode) {
 	buff[totalLen] = '\0';
 
 	return buff;
+}
+
+int receive(int sock, char * packet, struct sockaddr sock_addr, int sendsize) {
+	
+	printf("Hello\n");
+	int recvfromlen = recvfrom(sock, packet, BSIZE, 0, &sock_addr, &sendsize);
+	printf("Hello\n");
+	printf("%d\n", recvfromlen);
+
+	if (recvfromlen < 0) {
+      		perror ("Client: recvfrom has returned an error");
+      		exit(EXIT_FAILURE);
+      	}
+      	
+      	return recvfromlen;
+}
+
+char ACK(char * packet){
+
+	char ACK[ACKSize];
+	ACK[0] = 0;
+	ACK[1] = 4;
+	ACK[2] = packet[2];
+	ACK[3] = packet[3];
 }
 
